cd6e65f55f78c2dbbdf79d31159c161fd91b14db
ODavid <odavid@castlefineart.com>
2024-09-13 17:50:51 +0100

    [1] - Adjust side navigation

--- client/src/components/layout/navigation.tsx
+++ client/src/components/layout/navigation.tsx
@@ -12,30 +12,30 @@ type Props = {
 export default function Sidebar({ displaySidebar }: Props) {
   return (
     <div
-      className={`flex flex-col bg-[#2e2e30] w-[240px] flex-shrink-0 h-full pb-16 transition-all ease-in-out duration-300 ${
-        displaySidebar ? "translate-x-0" : "-translate-x-full"
+      className={`flex flex-col bg-[#2e2e30] w-[240px] flex-shrink-0 transition-all ease-in-out duration-200 ${
+        displaySidebar ? "w-[240px]" : "w-0"
       }`}
     >
-      <ul className="border-b border-[#424244] mb-3 pb-3 list-none ml-0 px-4">
-        <li className="text-white py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer">
+      <ul className={`mb-3 list-none ml-0 px-4 transition-all ease-in-out duration-[50] ${displaySidebar ? "opacity-100" : "opacity-0"}`}>
+        <li className="text-white py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer whitespace-nowrap">
           <Link to="/home" className="flex items-center gap-x-2 w-full">
             <Home width={16} height={16} />
             <span>Home</span>
           </Link>
         </li>
-        <li className="text-white w-full py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer">
+        <li className="text-white w-full py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer whitespace-nowrap">
           <Link to="/tasks/list" className="flex items-center gap-x-2">
             <ListTodo width={16} height={16} />
             <span>Tasks Manager</span>
           </Link>
         </li>
-        <li className="text-white py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer">
+        <li className="text-white py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer whitespace-nowrap">
           <Link to="/notes" className="flex items-center gap-x-2 w-full">
             <Notebook width={16} height={16} />
             <span>Notes</span>
           </Link>
         </li>
-        <li className="text-white  w-full py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer">
+        <li className="text-white  w-full py-1 px-4 hover:bg-[#3D3E40] rounded-lg cursor-pointer whitespace-nowrap">
           <Link to="/finance" className="flex items-center gap-x-2">
             <Landmark width={16} height={16} />
             <span>Finance Manager</span>
