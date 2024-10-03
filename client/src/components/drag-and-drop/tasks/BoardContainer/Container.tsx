c282d01c013971ac597d3b5fb7de501a14514d1a
ODavid <octaviandd@yahoo.com>
2024-10-03 22:43:31 +0100

    [master] - Board settings button

--- client/src/components/drag-and-drop/tasks/BoardContainer/Container.tsx
+++ client/src/components/drag-and-drop/tasks/BoardContainer/Container.tsx
@@ -11,8 +11,20 @@ import React, {
 import { Handle } from "../BoardItem/Handle";
 import { cn } from "@/utils/helpers/utils";
 import styles from "./container.module.scss";
-import { Plus } from "lucide-react";
+import { Plus, Ellipsis, Github, LogOut, Settings, User } from "lucide-react";
 import { UniqueIdentifier } from "@dnd-kit/core";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/layout/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { DropdownMenuShortcut } from "@/components/layout/dropdown-menu";
 
 export interface Props {
   children: React.ReactNode;
@@ -90,7 +102,7 @@ export const BoardContainer = forwardRef<HTMLDivElement, Props>(
           placeholder && styles.placeholder,
           scrollable && styles.scrollable,
           shadow && styles.shadow,
-          "border border-[#424244]"
+          "border border-[#424244] border-opacity-0 hover:border-opacity-100 transition-all duration-400 ease-in-out"
         )}
         onClick={onClick}
         tabIndex={onClick ? 0 : undefined}
@@ -104,10 +116,29 @@ export const BoardContainer = forwardRef<HTMLDivElement, Props>(
               ref={inputRef}
             ></input>
             <div className="flex items-center gap-x-3">
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between p-1 rounded-md hover:bg-[#343638]">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Ellipsis width={16} height={16} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    className="mt-2 bg-[#1E1F21] border border-neutral-600 rounded-md z-50"
+                    align="start"
+                    alignOffset={10}
+                  >
+                    <DropdownMenuItem className="cursor-pointer px-3 py-2 hover:bg-[#3D3E3F]">
+                      Rename Section
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="cursor-pointer px-3 py-2 hover:bg-[#3D3E3F]">
+                      Delete Section
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <div className="flex items-center justify-between p-1 rounded-md hover:bg-[#343638]">
                 <Plus width={16} height={16} />
               </div>
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-between p-1">
                 <Handle {...handleProps} />
               </div>
             </div>
