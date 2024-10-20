1bc00c7551d81cd0abb7fb6f6ca5a9d9827f74cb
ODavid <octaviandd@yahoo.com>
2024-10-20 12:49:52 +0100

    [master] - adjust scripts

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,8 +2,6 @@
 
 rm -rf /home/ec2-user/multiapp/*
 
-chmod +x scripts/clean_old_files.sh
-
 # Stop Node.js backend and React frontend
 pm2 stop my-node-backend || true
 pm2 stop my-react-client || true
