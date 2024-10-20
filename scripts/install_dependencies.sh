5138949aa2391fdb562b4a823b6f5928a281a9e7
ODavid <octaviandd@yahoo.com>
2024-10-20 13:42:44 +0100

    [master] - change permissions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo chown -R ec2-user:ec2-user /home/ec2-user/multiapp
+find /home/ec2-user/multiapp -type d -exec chmod 755 {} \;
+find /home/ec2-user/multiapp -type f -exec chmod 644 {} \;
+
 # Navigate to the backend root (where the index.ts and package.json are)
 cd /home/ec2-user/multiapp
 
