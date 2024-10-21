f1d243b0d9bcefe1621ef8d94a058f953b0c20bf
ODavid <octaviandd@yahoo.com>
2024-10-21 13:46:01 +0100

    [master] - adjust package json

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,6 +5,7 @@ cd /home/ec2-user/multiapp
 
 # Build the TypeScript project and start using PM2
 npm run build:prod
+npm run post:build
 pm2 start dist/index.js --name my-node-backend
 
 # Serve the React app (assuming it’s already built)
