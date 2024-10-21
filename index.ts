09f1ebc579f6a8b69b3e680e5a2a6e943a57b200
ODavid <octaviandd@yahoo.com>
2024-10-21 17:58:25 +0100

    [master] - logs'

--- index.ts
+++ index.ts
@@ -29,6 +29,11 @@ app.use(
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/boards", boardRoutes);
