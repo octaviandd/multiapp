febdd2a0308ef72abc530074ce03b15c243259ab
ODavid <octaviandd@yahoo.com>
2024-10-21 13:17:54 +0100

    [master] - unncessary controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 /** @format */
 
 import express from "express";
-import userController from "../controllers/user.controller";
+// import userController from "../controllers/user.controller";
 import { withAuth } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/me", withAuth, userController.getUserFromSession);
-router.get("/:id", withAuth, userController.getUser);
+// router.get("/me", withAuth, userController.getUserFromSession);
+// router.get("/:id", withAuth, userController.getUser);
 
 export default router;
