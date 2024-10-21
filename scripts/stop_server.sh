c2f12a00f8bae242882d6bec8c4b9adac0141675
ODavid <octaviandd@yahoo.com>
2024-10-21 17:41:52 +0100

    [master] - scripts hoppe

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,6 +2,7 @@
 
 echo "Current directory:  $(pwd)" 
 sudo chown -R ec2-user:ec2-user /home/ec2-user/multiapp
+sudo chown -R ec2-user:ec2-user /home/ec2-user/multiapp/client
 sudo rm -rf /home/ec2-user/multiapp/*
 
 # Stop Node.js backend and React frontend
