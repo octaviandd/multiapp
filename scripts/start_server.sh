0f26eb1aee0d16b90eefd2bda4ef3c35d4ab4129
ODavid <octaviandd@yahoo.com>
2024-10-21 15:26:36 +0100

    [master] - scripts'

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,12 +5,9 @@ cd /home/ec2-user/multiapp
 
 # Build the TypeScript project and start using PM2
 npm run build:prod
-npm run post:build
 pm2 start dist/index.js --name my-node-backend 
 
 # Serve the React app (assuming it’s already built)
 cd /home/ec2-user/multiapp/client
 
 npm run build
-
-Serve React using a static server like serve, nginx, or continue using React’s development server (for simplicity here):
