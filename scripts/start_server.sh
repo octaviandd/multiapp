7affba2a312048c200f609a8c29eccb1b4c2ba91
ODavid <octaviandd@yahoo.com>
2024-10-21 15:55:15 +0100

    [master] - global pm2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,4 +6,4 @@ cd /home/ec2-user/multiapp
 # Build the TypeScript project and start using PM2
 npm install
 npm run build:prod
-pm2 start dist/index.js --name my-node-backend 
+/usr/local/bin/pm2 start dist/index.js --name my-node-backend
