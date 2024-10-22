f39746ea6a331f2a5721f5621827cd2aaf76de91
Octavian <odavid@castlefineart.com>
2024-10-22 12:46:42 +0100

    [dev] - Dynamically set-up database env

--- src/prisma/prisma-client.ts
+++ src/prisma/prisma-client.ts
@@ -1,9 +1,37 @@
 /** @format */
 
 import { PrismaClient } from "@prisma/client";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "database_url";
+
+const client = new SecretsManagerClient({
+  region: "eu-west-2",
+});
+
+let response : any = null;
+
+(async () => {
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT",
+      })
+    );
+  } catch (error) {
+    throw error;
+  }
+})();
+
+const secret = response?.SecretString;
 
 const prisma = new PrismaClient({
   log: ["info", "warn", "error"],
+  datasourceUrl: secret
 });
 
 export default prisma;
