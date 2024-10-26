56e0a5f36716f261cb373c65e8a38ee010349759
ODavid <octaviandd@yahoo.com>
2024-10-26 11:48:19 +0100

    [master] - Config

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,19 +1,22 @@
+/** @format */
+
 import multer from "multer";
 import multerS3 from "multer-s3";
 import s3 from "./s3client";
+import { Request } from "express";
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME as string,
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
+    metadata: function (req: Request, file, cb) {
+      cb(null, { fieldName: file.fieldname });
     },
-    key: function (req, file, cb) {
+    key: function (req: Request, file, cb) {
       const uniqueFileName = Date.now().toString() + "-" + file.originalname;
       cb(null, uniqueFileName);
-    }
-  })
-})
+    },
+  }),
+});
 
 export default upload;
